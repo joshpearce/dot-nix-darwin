e3e6503ec9d52548aa0888b8a7f1e488041f5ea1
Josh Pearce <joshua.pearce@gmail.com>
2024-01-20 11:59:09 -0500

    added direnv

--- jjp4g/home.nix
+++ jjp4g/home.nix
@@ -22,6 +22,11 @@
         speedtest-cli
         
       ];
+      programs.direnv = {
+        enable = true;
+        enableZshIntegration = true;
+        nix-direnv.enable = true;
+      };
     };
   };
 }
